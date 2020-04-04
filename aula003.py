6c2676a6e4def3dd21dcbb33b6df02c848b79636
VAIO-MC3 <arc.mau@gmail.com>
2020-04-03 22:27:50 -0300

    commit

---
+++ aula003.py
@@ -0,0 +1,18 @@
+nome = input('Qual seu nome: ')
+idade = int(input('Quantos anos você tem? '))
+
+print('Olá ', nome, ', como vai, tudo bem? ')
+print('')
+print('Quantos convidados vão na sua festa de aniversário? ')
+n1 = int(input('Numero de convidados: '))
+n2 = int(input('Numero de pessoas trabalhando: '))
+s = n1 + n2
+n3 = int(2020)
+i = n3 - idade
+print('Vocẽ nasceu em {} e vai comemorar em um salão de festa que comporte {} pessoas!'.format(i, s))
+
+nome = input('Qual seu nome: ')
+print('Olá ', nome, ', como vai, tudo bem? ')
+n4=float(input('Digite um valor: '))
+print(n4)
+print(type(n4))
---
+++ aula_desafio_003.py
@@ -0,0 +1,7 @@
+# mostre dois numeros e mostre a soma entre eles
+n1 = int (input('digite um valor 01 '))
+n2 = int (input('digite um valor 02 '))
+s=n1+n2
+print('A soma de {} + {} é igual a {}' .format(n1,n2,s))
+
+# foi..
