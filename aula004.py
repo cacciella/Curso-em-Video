700730276ec30635aa5a1c9b3aa1fa9497d6e4ef
VAIO-MC3 <arc.mau@gmail.com>
2020-04-03 22:31:50 -0300

    commit

---
+++ aula004.py
@@ -0,0 +1,17 @@
+n =str( input('Digite um valor: '))
+# mostra se o valor é numerico, verdadeiro ou falso
+
+print('é numero? ')
+print(n.isnumeric())
+
+# mostra se o valor é letra, verdadeiro ou falso
+print('é letra? ')
+print(n.isalpha())
+
+# mostra se o valor é alfanumerico, verdadeiro ou falso
+print('é alfanumérico? ')
+print(n.isalnum())
+
+# mostra se tem letra maiuscula, verdadeiro ou falso
+print('tem letra maíuscula? ')
+print(n.isupper())
