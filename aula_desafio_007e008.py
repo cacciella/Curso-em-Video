4533dde18ed955994fd9cf42776253d913c0355a
VAIO-MC3 <arc.mau@gmail.com>
2020-04-04 17:37:33 -0300

    Calculo de Média

---
+++ aula_desafio_007e008.py
@@ -0,0 +1,13 @@
+#aula_desafio_007
+n1 = int(input('Nota prova 01: '))
+n2 = int(input('Nota prova 02: '))
+m = ((n1+n2)/2)
+print('Sua media final é: {}'.format(m))
+
+# aula_desafio_008
+alt=float(input("Qual é a sua altura(m)?: "))
+cm=(alt*100)
+mm=(alt*1000)
+print('Então sua altura é equivalente a {}cm \ne\nEquivalente a {}mm '.format(cm,mm))
+
+
