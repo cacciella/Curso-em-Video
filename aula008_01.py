cd033c2cbb67212eff66a9d63bd7e2d32aa0775a
VAIO-MC3 <arc.mau@gmail.com>
2020-04-07 17:01:11 -0300

    math import sqrt

---
+++ aula008_01.py
@@ -0,0 +1,16 @@
+# digitando import e precione a tecla( Ctrl )e aperte a ( barra de espaço ),
+#  para mostrar todas as funcionalidades
+
+from math import sqrt,floor
+
+num = int(input('Digite um numero: '))
+raiz = sqrt(num)
+print('A raiz quadrada de {} é igual a {:.2f}'.format(num,floor(raiz)))
+
+
+# exemplo: escrever 6,34 e mostrar somente o 6
+
+import math
+num = float(input('Escreva um numero inteiro, com duas casa decimais: '))
+valor = math.floor(num)
+print('O numero é: {}' .format(math.floor(valor)))
